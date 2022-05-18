bac28137623b55f246d9b21cdc8327ff5d48806d
Karol Błażejczyk <69108041+Szyszka947@users.noreply.github.com>
2022-05-18 20:42:16 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace R_INF
+{
+    internal class Program
+    {
+        static string Szyfruj(string tekst, int klucz)
+        {
+            char[,] plotek = new char[klucz, tekst.Length];
+
+            bool zmniejszaj = false, zwiekszaj = true;
+
+            for (int i = 0, w = 0; i < tekst.Length; i++)
+            {
+                if (w == klucz - 1)
+                {
+                    zmniejszaj = true;
+                    zwiekszaj = false;
+                }
+                else if (w == 0 && i != 0)
+                {
+                    zmniejszaj = false;
+                    zwiekszaj = true;
+                }
+
+                plotek[w, i] = tekst[i];
+
+                if (zwiekszaj) w++;
+                else if (zmniejszaj) w--;
+            }
+
+            string szyfrogram = string.Empty;
+
+            foreach (var item in plotek)
+            {
+                szyfrogram += item;
+            }
+
+            return szyfrogram;
+        }
+
+        static void Main(string[] args)
+        {
+            var x = Szyfruj("KRYPTOGRAFIA", 4);
+            Console.WriteLine(x);
+        }
+    }
+}
